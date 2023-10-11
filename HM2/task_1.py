05f32a3542adf8957fe275ff9a2518dbe474e79e
NELETOOOO <lera-zima15@mail.ru>
2023-10-11 23:36:44 +0500

    added task1

---
+++ HM2/task_1.py
@@ -0,0 +1,11 @@
+slovo1 = input(f'Введите слово на латинице  /n ')
+slovo1_len = len(slovo1)
+idx = slovo1_len // 2
+
+if slovo1_len % 2 == 0:
+    print(slovo1[idx - 1:idx + 1] ) 
+else:
+    print(slovo1[idx])  
+        
+    
+    
